d3193601f0bf451f5ec5c58dc92e9283c50bbed6
Miguel Veloso <mvelosop@gmail.com>
2021-07-22 21:49:10 +0100

    Add standard Serilog + Seq + Application Insights configuration

---
+++ src/SerilogWebApi.DotNetCore31/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace SerilogWebApi.DotNetCore31
+{
+    public class AssemblyInfo
+    {
+        public static Assembly Assembly => typeof(AssemblyInfo).Assembly;
+
+        public static string AssemblyName => typeof(AssemblyInfo).Assembly.GetName().Name;
+
+        public static string Version => Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+    }
+}
---
+++ src/SerilogWebApi.DotNetCore31/Configuration/LoggingConfigurationExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Destructurama;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace SerilogWebApi.DotNetCore31.Configuration
+{
+    public static class LoggingConfigurationExtensions
+    {
+        public static LoggerConfiguration ConfigureSerilogDefaults(this LoggerConfiguration loggerConfiguration, string seqUrl, string appName, string hostName, IHostEnvironment hostEnvironment = null)
+        {
+            loggerConfiguration
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", appName)
+                .Enrich.WithProperty("HostName", hostName)
+                .Destructure.JsonNetTypes() // Enable JObject destruturing
+                .WriteTo.Console()
+                .WriteTo.File( // Write to standard Azure AppService-monitored log location
+                    $@"D:\home\LogFiles\{appName}-.log",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 7,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1));
+
+            var environment = hostEnvironment is null
+                ? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                : hostEnvironment.EnvironmentName;
+
+            // Add Seq for development by default or always if configured
+            if (string.IsNullOrWhiteSpace(seqUrl))
+            {
+                if (string.Equals(environment, "Development", StringComparison.OrdinalIgnoreCase))
+                {
+                    loggerConfiguration.WriteTo.Seq("http://localhost:5341");
+                }
+            }
+            else
+            {
+                loggerConfiguration.WriteTo.Seq(seqUrl);
+            }
+
+            return loggerConfiguration;
+        }
+    }
+}
